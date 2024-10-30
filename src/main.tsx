47001da2da25fed4fbb66146ab63820a6e1aa81b
rguthier <rkg7460@gmail.com>
2024-10-30 17:43:56 -0400

    project init with vite

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root"
+import ErrorPage from "./error";
+import BrowseBooks from "./routes/browse";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/browse",
+        element: <BrowseBooks />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/routes/browse.tsx
@@ -0,0 +1,5 @@
+export default function BrowseBooks() {
+    return (
+        <h1>Will get data from database and put it here</h1>
+    )
+}

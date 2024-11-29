8a121d8605a325f4d1f7f92927584d4b03a379de
Areli Morales <55932438+AreliMH@users.noreply.github.com>
2024-11-29 17:13:46 -0500

    Add updated folder

---
+++ .Trash/PublicsLibrary 17-12-03-743/PublicsLibrary-books-updates/client/src/pages/Footer.tsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+const Footer = () => (
+  <footer>
+    <div className="footer-content" style={{ backgroundColor: "#61a876" }}>
+      <p>&copy; 2024 BookTrade. All rights reserved.</p>
+      <div className="social-links"></div>
+    </div>
+  </footer>
+);
+
+export default Footer;

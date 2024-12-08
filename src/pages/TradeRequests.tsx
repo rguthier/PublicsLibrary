0299faccd1a3a934eb247182e932f17cdfab47f7
rguthier <rkg7460@gmail.com>
2024-12-07 20:39:18 -0500

    Remove duplicate files

--- src/pages/TradeRequests.tsx
+++
@@ -1,8 +0,0 @@
-export default function TradeRequests() {
-  return (
-    <div className="trade-requests">
-      <h1>Trade Requests</h1>
-      <p>Manage and view incoming and outgoing trade requests.</p>
-    </div>
-  );
-}

365bcf90a0f58ac154e270b762f64588c341952d
Ayomide Babatunde <ababatu1@students.towson.edu>
2024-12-08 16:40:28 -0500

    search endpoint added

--- src/server/server.js
+++ src/server/server.js
@@ -15,7 +15,7 @@ database.connect((err) => {
   console.log("Database connected");
 });
 
-//endpoints
+
 app.get("/", (req, res) => {
   res.send("Working");
 });
@@ -67,4 +67,34 @@ app.post("/addbook", (req, res) => {
   );
 });
 
-app.listen(3000);
+
+app.get("/search", (req, res) => {
+  const zipcode = req.query.zipcode;
+
+  if (!zipcode) {
+    return res.status(400).json({ error: "Please provide a zipcode." });
+  }
+
+  
+  const query = `
+    SELECT * FROM book 
+    JOIN user ON book.owner_id = user.user_id
+    WHERE user.zipcode = ?`;
+
+  database.query(query, [zipcode], (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err.message);
+      return res.status(500).json({ error: "Database error." });
+    }
+
+    if (results.length > 0) {
+      res.json(results); 
+    } else {
+      res.status(404).json({ message: "No books found for this zipcode." });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
